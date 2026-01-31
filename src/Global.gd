1e3f7c9fa433aaa3d910639b49965ff4c40434a5
ExtraTNT <56388556+ExtraTNT@users.noreply.github.com>
2026-01-31 12:11:40 +0100

    add health hearts bar hugi

--- src/Global.gd
+++ src/Global.gd
@@ -8,7 +8,7 @@ signal player_dead
 var mask_on := false
 var sanity_value := 0 #Max 100
 var current_level := 0
-var player_health #TBD
+var player_health := 6#TBD
 
 
 func on_mask_switch():
@@ -21,7 +21,6 @@ func on_player_dead():
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
