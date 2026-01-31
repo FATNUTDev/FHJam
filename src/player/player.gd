29009577187696caf3ff9329c6d56fe75d041f7f
NikitaSosnovski <nikita.sosnovski.11@gmail.com>
2026-01-31 11:33:50 +0100

    init

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,14 +1,22 @@
 extends CharacterBody2D
 
+@onready var sanity_timer = $Timer
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -200.0
+ 
 
 func _ready() -> void:
-	pass
-	#connect("switch_to_mask_mode", on_mask_switch_signal)
+	Global.switch_mask_mode.connect(on_mask_switch_signal)
+	Global.player_dead.connect(on_player_dead_signal)
+
+func check_player_stats():
+	if Global.sanity_value > 99:
+		Global.on_player_dead()
 
 func _physics_process(delta: float) -> void:
+	#Check Player sanity and health
+	check_player_stats()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -28,14 +36,25 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	pick_mask()
 
-
 func pick_mask():
 	if Input.is_action_just_pressed("switch_mask"):
 		Global.mask_on = !Global.mask_on
 		Global.on_mask_switch()
+		if Global.mask_on:
+			Global.sanity_value += 1
+			sanity_timer.start()
+		else:
+			sanity_timer.stop()
 
 func on_collision():
 	pass
 
 func on_mask_switch_signal():
 	print("player switched")
+	
+func on_player_dead_signal():
+	print("Game Over")
+
+func _on_sanity_timer_timeout() -> void:
+	Global.sanity_value += 1
+	print(Global.sanity_value)
