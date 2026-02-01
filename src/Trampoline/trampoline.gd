5c8ee689943facf64a10c057a8fe27fd05038723
NikitaSosnovski <nikita.sosnovski.11@gmail.com>
2026-02-01 12:04:48 +0100

    modified:   src/Trampoline/Trampoline.tscn 	modified:   src/Trampoline/trampoline.gd 	new file:   src/levels/level2.tscn

--- src/Trampoline/trampoline.gd
+++ src/Trampoline/trampoline.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+@onready var animation_pl = $AnimationPlayer
+var reacted = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +15,12 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		animation_pl.play("reaction")
 		body.on_trampoline_collision()
+		reacted = true
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if reacted:
+		animation_pl.play_backwards("reaction")
+		reacted = false
